50488cb2fc4e49c2af9c23ce25ba5050d47b297a
Vitor Machado Vieira <vmvieira55@gmail.com>
2020-11-02 16:23:49 -0300

    Finished the bounce function

--- index.js
+++ index.js
@@ -3,46 +3,120 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-class Boxes {
-  constructor(x, y, width, height, speed, gameMode) {
+// ctx.fillStyle = "blue";
+// ctx.fillRect(50, 50, 400, 50);
+// ctx.fillStyle = "black";
+// ctx.fillRect(300, canvas.height - 50, 400, 50);
+
+class Game {
+  constructor(topBox, botBox) {
+    this.topBox = topBox;
+    this.botBox = botBox;
+    this.animationId;
+    this.frames = 0;
+    this.score = 0;
+  }
+
+  updateGame = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.topBox.draw();
+    this.topBox.bounce();
+    this.botBox.draw();
+    //this.updateScore(this.score)
+    this.animationId = requestAnimationFrame(this.updateGame);
+  };
+}
+
+class Box {
+  constructor(x, y, width, height, xSpeed, ySpeed, gameMode) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    this.speed = speed;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
     this.gameMode = gameMode;
   }
-}
 
-class Game {
-  constructor(score, frames, animationId) {
-    this.score = score;
-    this.frames = frames;
-    this.animationId;
+  bounce() {
+    if (this.gameMode == "bounce") {
+      this.x += this.xSpeed;
+    }
+    if (this.xSpeed > 0 && this.x + this.width > canvas.width) {
+      this.xSpeed = -this.xSpeed;
+    }
+    if (this.xSpeed < 0 && this.x < 0) {
+      this.xSpeed = -this.xSpeed;
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
-// Função que inicia o jogo, inserindo as caixas e o placar.
-function startGame() {
-  const game = new Game();
-  const box = new Boxes();
-}
+window.onload = () => {
+  document.getElementById("canvas").onclick = () => {
+    startGame();
+  };
 
-// Função que anima o jogo, atualizando o lugar de cada elemento.
-function updateGame() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  requestAnimationFrame;
-}
+  function startGame() {
+    const game = new Game(
+      (topBox = new Box(50, 50, 400, 50, 2, 0, "bounce")),
+      (botBox = new Box(300, canvas.height - 50, 400, 50, "static"))
+    );
 
-// O core loop do jogo:
+    game.updateGame();
+  }
+};
 
-// Função que fica pingando a caixa nas laterais do canvas.
-function bounceMode() {}
+//     startGame() {
+//       let activeBox = new Boxes(50, 50, 400, 50, 1, 0, "bounce");
+//       let staticBox = new Boxes(300,canvas.height - 50, 400, 50);
 
-// Função que solta a caixa do "bounceMode" e detecta ou não a colisão entre elas.
-function releaseMode() {}
+//       updateGame();
+//     }
+//     Função que anima o jogo, atualizando o lugar de cada elemento.
 
-// Função que faz todas as verificações:
-// Verifica se o jogo encerrou "gameover" e, caso contrário,
-// Cria nova caixa, atualiza o placar e retorna o gameMode para o bounceMode
-function checkMode() {}
+//     updateGame() {
+//       ctx.clearRect(0, 0, canvas.width, canvas.height);
+//       this.Boxes.draw();
+//       requestAnimationFrame;
+//     }
+//     draw() {
+//       ctx.fillStyle = "blue";
+//       ctx.fillRect(this.x, this.y, this.width, this.height);
+//     }
+
+//     O core loop do jogo:
+
+//     Função que fica pingando a caixa nas laterais do canvas.
+//     bounceMode() {
+//       Possível versão da função:
+//       if (gameMode == 'bounce') {
+//         this.xSpeed = 1;
+//         if (this.xSpeed > 0 && this.x + this.width > canvas.width) {
+//           this.xSpeed = -1;
+//           }
+//         if (this.xSpeed < 0 && this.X < 0) {
+//           this.xSpeed = 1;
+//           }
+//       }
+//     }
+
+//     Função que solta a caixa do "bounceMode" e detecta ou não a colisão entre elas.
+//     releaseMode() {}
+
+//     Função que faz todas as verificações:
+//     Verifica se o jogo encerrou "gameover" e, caso contrário,
+//     Cria nova caixa, atualiza o placar e retorna o gameMode para o bounceMode
+//     checkMode() {}
+//   }
+
+//   class Game {
+//     constructor(score, frames, animationId) {
+//       this.score = score;
+//       this.frames = frames;
+//       this.animationId;
+//     }
